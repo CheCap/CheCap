dea17f8744b2985e0bd8d3bb2bbe0278d414239e
EATER-LETS\Eater <g.m.eater34@gmail.com>
2018-02-23 23:46:04 +0900

    PC側のPythonのコードを追加 ファイル名を変更しフォルダを整理

---
+++ device/software/UDPytest/udptest.py
@@ -0,0 +1,28 @@
+# -*- coding: cp932 -*-
+from __future__ import print_function
+import socket
+import time
+from contextlib import closing
+
+def main():
+    host = '192.168.200.200' # IPアドレス
+    port = 10000 # ポート番号
+    port2=10001
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #with closing(sock):
+    sock.bind(('192.168.200.233',port2))
+    while True:
+        message = 'A'.encode('utf-8') # 送る文字列(ここでは「A」という文字列を送っている)
+        #print(message)
+        sock.sendto(message, (host, port))
+        #time.sleep(1)
+        try:
+          data=sock.recvfrom(1024)
+        except socket.error:
+          pass
+        else:
+          print("rcvd:", data)
+    return
+
+if __name__ == '__main__':
+  main()
