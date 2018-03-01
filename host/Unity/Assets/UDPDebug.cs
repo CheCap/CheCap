be1ac1fbf453c23e1cd2484d5e36b370be589b96
EATER-LETS\Eater <g.m.eater34@gmail.com>
2018-03-01 18:18:29 +0900

    UnityでUDP受信に成功(UDP_Thread.cs) UDPDebug - UDPReceiverRxはUniRxを使ったスクリプトだがNullReferenceExceptionで動作せず

---
+++ host/Unity/Assets/UDPDebug.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UdpReceiverUniRx;
+
+public class UDPDebug : MonoBehaviour {
+	public UdpReceiverRx _udpReceiverRx;
+	private IObservable<UdpState> myUdpSequence;
+
+	TextMesh Box;
+
+	// Use this for initialization
+	void Start () {
+		//myUdpSequence = _udpReceiverRx._udpSequence;
+		Box = GetComponent<TextMesh> ();
+
+		/*myUdpSequence*/_udpReceiverRx._udpSequence
+			.ObserveOnMainThread()
+			.Subscribe(x =>{
+				print(x.UdpMsg);
+				Box.text=x.UdpMsg;
+			})
+			.AddTo(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ host/Unity/Assets/UdpReceiverRx.cs
@@ -0,0 +1,80 @@
+﻿//UdpReceiverRx.cs
+//UniRxを用いて高速なUdp受信を行う
+
+namespace UdpReceiverUniRx {
+
+	using UnityEngine;
+	using System.Net;
+	using System.Net.Sockets;
+	using UniRx;
+
+	public class UdpState : System.IEquatable<UdpState>
+	{
+		//UDP通信の情報を収める。送受信ともに使える
+		public IPEndPoint EndPoint {get; set;}
+		public string UdpMsg {get; set;}
+
+		public UdpState(IPEndPoint ep, string udpMsg)
+		{
+			this.EndPoint = ep;
+			this.UdpMsg = udpMsg;
+		}
+		public override int GetHashCode() {
+			return EndPoint.Address.GetHashCode();
+		}
+
+		public bool Equals(UdpState s)
+		{
+			if ( s == null ) {
+				return false;
+			}
+			return EndPoint.Address.Equals(s.EndPoint.Address);
+		}
+	}
+
+	public class UdpReceiverRx : MonoBehaviour
+	{
+		private const int listenPort = 10001;
+		private static UdpClient myClient;
+		private bool isAppQuitting;
+		public IObservable<UdpState> _udpSequence;
+
+		void Awake()
+		{
+			Debug.Log ("Awake");
+			_udpSequence = Observable.Create<UdpState> (observer => {
+				Debug.Log ("Obs");
+				Debug.Log (string.Format ("_udpSequence thread: {0}", System.Threading.Thread.CurrentThread.ManagedThreadId));
+				try {
+					myClient = new UdpClient (listenPort);
+				} catch (SocketException ex) {
+					observer.OnError (ex);
+				}
+				IPEndPoint remoteEP = null;
+				myClient.EnableBroadcast = true;
+				myClient.Client.ReceiveTimeout = 50000;
+				while (!isAppQuitting) {
+					try {
+						remoteEP = null;
+						var receivedMsg = System.Text.Encoding.ASCII.GetString (myClient.Receive (ref remoteEP));
+						observer.OnNext (new UdpState (remoteEP, receivedMsg));
+					} catch (SocketException) {
+						Debug.Log ("UDP::Receive timeout");
+					}
+				}
+				observer.OnCompleted ();
+				return null;
+				//return Disposable.Empty;
+			});
+				//.SubscribeOn(Scheduler.ThreadPool)
+				//.Publish()
+				//.RefCount();
+		}
+
+		void OnApplicationQuit()
+		{
+			isAppQuitting = true;
+			myClient.Client.Blocking = false;
+		}
+	}
+}
