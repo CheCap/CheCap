3454ae525e8f880d422944b53df58db7adb68712
EATER-LETS\Eater <g.m.eater34@gmail.com>
2018-04-23 02:30:12 +0900

    コミット漏れ？

---
+++ device/software/GetMotion/GetMotion.ino
@@ -0,0 +1,68 @@
+#include "freeram.h"
+
+#include "mpu.h"
+#include "I2Cdev.h"
+
+#include "Encode.h"
+
+uint8_t led =13;
+
+
+
+int ret;
+void setup() {
+    pinMode(led,OUTPUT);
+    Wire.begin();
+    Serial.begin(9600/*,SERIAL_8O1*/);
+    ret = mympu_open(200);
+	
+}
+
+unsigned int c = 0; //cumulative number of successful MPU/DMP reads
+unsigned int np = 0; //cumulative number of MPU/DMP reads that brought no packet back
+unsigned int err_c = 0; //cumulative number of MPU/DMP reads that brought corrupted packet
+unsigned int err_o = 0; //cumulative number of MPU/DMP reads that had overflow bit set
+
+void loop() {
+  //uint16_t checksum=0;
+  uint8_t plaindata[16];  //long型のクォータニオンを1バイトに分割したデータ
+  uint8_t udata[33];       //送りたいデータ(↑)をエンコードした、最終的にUARTで送るデータ
+  uint8_t numofdata;       //送信バイト数
+  
+  //ret = mympu_update();
+  
+  digitalWrite(led,HIGH);
+  ret = lmympu_update();
+  switch (ret) {
+	case 0: c++; break;
+	case 1: np++; //return;
+	case 2: err_o++; //return;
+	case 3: err_c++; //return; 
+	//default: 
+		//Serial.print("READ ERROR!  ");
+		//Serial.println(ret);
+    digitalWrite(led,LOW);
+		//return;
+  }
+  lmympu.lq[0]=305419896;
+  lmympu.lq[1]=305419896;
+  lmympu.lq[2]=305419896;
+  lmympu.lq[3]=305419896;
+    
+ for(uint8_t i=0; i<4; i++){
+  longToByte(lmympu.lq[i],&plaindata[i*4]);
+ }
+
+  numofdata=encodeData(plaindata,16,udata);
+  Serial.write(udata,numofdata);
+  /*Serial.print(lmympu.lq[0]/1073741824.f);
+  Serial.print("\t");
+  Serial.print(lmympu.lq[1]/1073741824.f);
+  Serial.print("\t");
+  Serial.print(lmympu.lq[2]/1073741824.f);
+  Serial.print("\t");
+  Serial.print(lmympu.lq[3]/1073741824.f);
+  Serial.println("");*/
+  //delay(10);
+}
+
---
+++ device/software/SerialToWiFi/SerialToWiFi.ino
@@ -0,0 +1,135 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include "Encode.h"
+
+#define CONDEBUG    //つながれたLEDでPCとの接続をデバッグする
+#define LED 23
+#define UBUFF 18
+
+HardwareSerial Serial2(2);
+
+
+const char ssid[] = "ESP32_wifi"; // SSID
+const char pass[] = "esp32pass";  // password
+const int localPort = 10000;      // ポート番号(pythonのポート番号に合わせる)
+const int remotePort = 10001;
+
+const IPAddress ip(192, 168, 200, 232);       // IPアドレス(pythonのIPアドレスに合わせる)
+const IPAddress remoteIP(192,168,200,233);  //上のと連番じゃないとdhcp環境で動かない
+const IPAddress subnet(255, 255, 255, 0); // サブネットマスク
+
+WiFiUDP udp;
+
+long quat[4];//UARTで受信したデータをデコードしてlong型のクォータニオンにしたデータ
+uint8_t uartBuff[UBUFF];//UARTで受信したデータ
+uint8_t pBuff=0;
+uint8_t pflag =0;
+int8_t numofdata=0;
+
+void beginUDP(){
+#ifdef CONDEBUG
+  pinMode(LED,OUTPUT);  
+#endif
+
+  WiFi.softAP(ssid, pass);           // SSIDとパスの設定
+  delay(100);                        // 追記：このdelayを入れないと失敗する場合がある
+  WiFi.softAPConfig(ip, ip, subnet); // IPアドレス、ゲートウェイ、サブネットマスクの設定
+
+  IPAddress myIP = WiFi.softAPIP();
+  
+  udp.begin(localPort);  // UDP通信の開始(引数はポート番号)
+  
+}
+
+void transmitUDP(/*long*/uint8_t* txdata, uint8_t sizeofdata){
+  udp.beginPacket(remoteIP, remotePort);
+  if(sizeofdata>0){
+    for(uint8_t i=0; i<sizeofdata; i++){
+      udp.write(txdata[i]);
+      Serial.print(txdata[i]);
+      Serial.print(",");
+    }
+    Serial.println("");
+  }else{
+    udp.print("ERR");
+    udp.print(numofdata);
+  }
+  udp.endPacket();
+}
+
+void checkUART(){
+  uint8_t dedata[UBUFF];        //受信したデータをデコードしたデータ
+  while(Serial2.available() > 0){
+    switch(pflag){
+    case 0:
+      uartBuff[0]=Serial2.read();
+      if(uartBuff[0] == START){
+        pflag=1;
+        pBuff=1;
+      }
+      break;
+    case 1:
+      uartBuff[pBuff] = Serial2.read();
+      if(uartBuff[pBuff] == START && uartBuff[pBuff-1] != ESC){
+        numofdata=decodeData(uartBuff,pBuff,dedata);
+        //for(uint8_t i=0; i<4; i++)
+        //  byteToLong(&dedata[i*4],&quat[i]);
+        
+        transmitUDP(dedata,16);
+        uartBuff[0] = START;
+        pflag = 1;
+        pBuff = 0;
+      }
+      pBuff++;
+      if(pBuff>=UBUFF){
+        pBuff = 0;
+        pflag = 0;
+      }
+      break;
+    }
+  }
+
+}
+
+void SerialToWiFi(){
+  checkUART();
+  //while(Serial2.available()>0) Serial2.read();
+  /*while(Serial2.available() > 0){
+    udpTx[0] = Serial2.read();
+    Serial2.write(udpTx[0]);
+  }*/
+
+}
+
+
+void setup() {
+  delay(100);
+  Serial.begin(9600);
+  Serial2.begin(9600/*,SERIAL_8O1*/);
+  beginUDP();
+
+}
+
+void loop() {
+  if(WiFi.softAPgetStationNum() > 0){
+#ifdef CONDEBUG
+    digitalWrite(LED,HIGH);  
+#endif
+ 
+    udp.beginPacket(remoteIP, remotePort);
+    /*quat[0]=0;
+    quat[1]=0;
+    quat[2]=0;
+    quat[3]=0;
+    transmitUDP(quat,4);*/
+    udp.endPacket();
+    
+    while(WiFi.softAPgetStationNum() > 0){
+      SerialToWiFi();
+    }
+  }
+#ifdef CONDEBUG
+    digitalWrite(LED,LOW);  
+#endif 
+  
+}
